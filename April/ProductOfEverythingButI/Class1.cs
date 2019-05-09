a69a55a66f09489406c1dc4c583ee55ff777e00b
Eoin Ward <eward@microsoft.com>
2019-05-09 10:40:37 +0100

    0(n) without division

--- April/ProductOfEverythingButI/Class1.cs
+++ April/ProductOfEverythingButI/Class1.cs
@@ -54,22 +54,60 @@ namespace ProductOfEverythingButI
 
         public static int[] CalculateProductOfEverythingButIPreAnPost(int[] input)
         {
-
             int[] output = new int[input.Length];
-
-            int total = 1;
+            int[] pre = Pre(input);
+            int[] post = Post(input);
 
             for (int i = 0; i < input.Length; i++)
             {
-                total = input[i] * total;
+                if (i == 0)
+                {
+                    output[i] = post[i + 1];
+                }
+                else if (i == input.Length - 1)
+                {
+                    output[i] = pre[i - 1];
+                }
+                else
+                {
+                    output[i] = pre[i - 1] * post[i + 1];
+                }
             }
-
-            for (int i = 0; i < output.Length; i++)
-            {
-                output[i] = total / input[i];
-            }
-
             return output;
+        }
+
+        public static int[] Pre(int[] input)
+        {
+            int[] pre = new int[input.Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i == 0)
+                {
+                    pre[i] = input[i];
+                }
+                else
+                {
+                    pre[i] = pre[i-1] * input[i];
+                }
+            }
+            return pre;
+        }
+
+        public static int[] Post(int[] input)
+        {
+            int[] post = new int[input.Length];
+            for ( int i = input.Length - 1; i > -1; i--)
+            {
+                if (i == input.Length -1)
+                {
+                    post[i] = input[i];
+                }
+                else
+                {
+                    post[i] = input[i] * post[i + 1];
+                }
+            }
+            return post;
         }
 
 
--- April/ProductOfEverythingButIUnitTests/UnitTest1.cs
+++ April/ProductOfEverythingButIUnitTests/UnitTest1.cs
@@ -28,6 +28,29 @@ namespace ProductOfEverythingButIUnitTests
             // using https://github.com/jamesfoster/DeepEqual
             Assert.IsTrue(expectedResult.IsDeepEqual(actualResult));
         }
+
+
+        [TestMethod]
+        public void ProductOfEverythingButIPrePostTest1()
+        {
+            int[] input = { 1, 2, 3, 4, 5 };
+            int[] expectedResult = { 120, 60, 40, 30, 24 };
+
+            int[] actualResult = Class1.CalculateProductOfEverythingButIPreAnPost(input);
+            // using https://github.com/jamesfoster/DeepEqual
+            Assert.IsTrue(expectedResult.IsDeepEqual(actualResult));
+        }
+
+        [TestMethod]
+        public void ProductOfEverythingButIPrePostTest2()
+        {
+            int[] input = { 3, 2, 1 };
+            int[] expectedResult = { 2, 3, 6 };
+
+            int[] actualResult = Class1.CalculateProductOfEverythingButIPreAnPost(input);
+            // using https://github.com/jamesfoster/DeepEqual
+            Assert.IsTrue(expectedResult.IsDeepEqual(actualResult));
+        }
     }
 }
 
