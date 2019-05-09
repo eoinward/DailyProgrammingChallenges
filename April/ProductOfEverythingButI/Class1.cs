3815790ced1d86b99bd649f976879236f862d98e
Eoin Ward <eward@microsoft.com>
2019-05-09 10:54:37 +0100

    update comments

--- April/ProductOfEverythingButI/Class1.cs
+++ April/ProductOfEverythingButI/Class1.cs
@@ -1,5 +1,12 @@
 ﻿using System;
 
+/// <summary>
+/// Good morning! Here's your coding interview problem for today.
+///This problem was asked by Uber.
+///Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
+///For example, if our input was[1, 2, 3, 4, 5], the expected output would be[120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be[2, 3, 6].
+///Follow-up: what if you can't use division?
+/// </summary>
 namespace ProductOfEverythingButI
 {
     public class Class1
@@ -52,6 +59,18 @@ namespace ProductOfEverythingButI
             return output;
         }
 
+
+        /// <summary>
+        /// for the Unit test [1, 2, 3, 4, 5] input
+        /// pre will be [1, 2, 6, 24, 120]
+        /// post will be [120, 120, 60, 20, 5]
+        /// And for i you will multiple the pre i-1 and the post i+1 to get the i value for the output
+        /// Specail cases on the end where there is no multiple
+        /// 
+        /// 3 loops but none are nested to its 0(n)
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
         public static int[] CalculateProductOfEverythingButIPreAnPost(int[] input)
         {
             int[] output = new int[input.Length];
